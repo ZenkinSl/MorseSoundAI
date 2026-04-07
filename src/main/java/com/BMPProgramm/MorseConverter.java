2e0cdb76664d89a9ff2589d6c9f31415e363c62b
Harald1978Z <zenkin09@inbox.ru>
2026-04-07 22:40:54 +0300

    Update MorseConverter.java

--- src/main/java/com/BMPProgramm/MorseConverter.java
+++ src/main/java/com/BMPProgramm/MorseConverter.java
@@ -7,18 +7,52 @@ public class MorseConverter {
     private static final Map<Character, String> MORSE = new HashMap<>();
 
     static {
-        MORSE.put('A', ".-");
-        MORSE.put('B', "-...");
-        MORSE.put('C', "-.-.");
-        MORSE.put('D', "-..");
-        MORSE.put('E', ".");
         MORSE.put(' ', "/");
+        MORSE.put('a',".-");
+        MORSE.put('b',"-...");
+        MORSE.put('c', "-.-.");
+        MORSE.put('d', "-..");
+        MORSE.put('e', ".");
+        MORSE.put('f', "..-.");
+        MORSE.put('g', "--.");
+        MORSE.put('h', "....");
+        MORSE.put('i', "..");
+        MORSE.put('j', ".---");
+        MORSE.put('k', "-.-");
+        MORSE.put('l', ".-..");
+        MORSE.put('m', "--");
+        MORSE.put('n', "-.");
+        MORSE.put('o', "---");
+        MORSE.put('p', ".--.");
+        MORSE.put('q', "--.-");
+        MORSE.put('r', ".-.");
+        MORSE.put( 's',"...");
+        MORSE.put('t', "-");
+        MORSE.put('u', "..-");
+        MORSE.put('v', "...-");
+        MORSE.put('w', ".--");
+        MORSE.put('x', "-..-");
+        MORSE.put('y', "-.--");
+        MORSE.put('z', "--..");
+        MORSE.put('0', "-----");
+        MORSE.put('1', ".----");
+        MORSE.put('2', "..---");
+        MORSE.put('3', "...--");
+        MORSE.put('4', "....-");
+        MORSE.put('5', ".....");
+        MORSE.put('6', "-....");
+        MORSE.put('7', "--...");
+        MORSE.put('8', "---..");
+        MORSE.put('9', "----.");
+        MORSE.put('.', ".-.-");
+        MORSE.put(',', "--..--");
+        MORSE.put('?', "..--..");
     }
 
     public static String toMorse(String text) {
         StringBuilder result = new StringBuilder();
 
-        for (char c : text.toUpperCase().toCharArray()) {
+        for (char c : text.toLowerCase().toCharArray()) {
             result.append(MORSE.getOrDefault(c, "")).append(" ");
         }
 
